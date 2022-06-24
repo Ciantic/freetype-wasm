a0ce363a9619c6220b34ec8b6b2e8c9e4873cf63
Jari O. O. Pennanen <ciantic@oksidi.com>
2022-06-24 16:39:39 +0300

    format

--- example/example.js
+++ example/example.js
@@ -1,6 +1,6 @@
 // @ts-check
-import FreetypeInit from "../dist/freetype.js";
-const Freetype = await FreetypeInit();
+import FreetypeInit from "../dist/freetype.js"
+const Freetype = await FreetypeInit()
 
 /**
  * @typedef {Object} DrawCacheEntry
@@ -17,10 +17,10 @@ const Freetype = await FreetypeInit();
  * @returns {Promise<import("../dist/freetype.js").FT_FaceRec[]>}
  */
 async function createFontFromUrl(url) {
-  const font = await fetch(url);
-  const buffer = await font.arrayBuffer();
-  const face = Freetype.LoadFontFromBytes(new Uint8Array(buffer));
-  return face;
+    const font = await fetch(url)
+    const buffer = await font.arrayBuffer()
+    const face = Freetype.LoadFontFromBytes(new Uint8Array(buffer))
+    return face
 }
 
 /**
@@ -31,11 +31,11 @@ async function createFontFromUrl(url) {
  * @returns {Promise<import("../dist/freetype.js").FT_FaceRec[]>}
  */
 async function createGoogleFont(fontName, index = 0) {
-  const url = `https://fonts.googleapis.com/css?family=${fontName}`;
-  const css = await fetch(url);
-  const text = await css.text();
-  const urls = [...text.matchAll(/url\(([^\(\)]+)\)/g)].map((m) => m[1]);
-  return await createFontFromUrl(urls[index]);
+    const url = `https://fonts.googleapis.com/css?family=${fontName}`
+    const css = await fetch(url)
+    const text = await css.text()
+    const urls = [...text.matchAll(/url\(([^\(\)]+)\)/g)].map((m) => m[1])
+    return await createFontFromUrl(urls[index])
 }
 
 /**
@@ -45,28 +45,28 @@ async function createGoogleFont(fontName, index = 0) {
  * @param {DrawCache} cache
  */
 async function updateCache(str, cache) {
-  // Get char codes without bitmaps
-  const codes = [];
-  for (const char of new Set(str)) {
-    const point = char.codePointAt(0);
-    if (!cache.has(char) && point !== undefined) {
-      codes.push(point);
+    // Get char codes without bitmaps
+    const codes = []
+    for (const char of new Set(str)) {
+        const point = char.codePointAt(0)
+        if (!cache.has(char) && point !== undefined) {
+            codes.push(point)
+        }
     }
-  }
 
-  // Populate missing bitmaps
-  const newGlyphs = Freetype.LoadGlyphs(codes, Freetype.FT_LOAD_RENDER);
-  for (const [code, glyph] of newGlyphs) {
-    const char = String.fromCodePoint(code);
-    cache.set(char, {
-      glyph,
-      bitmap: glyph.bitmap.imagedata
-        ? await createImageBitmap(glyph.bitmap.imagedata)
-        : null,
-    });
-  }
+    // Populate missing bitmaps
+    const newGlyphs = Freetype.LoadGlyphs(codes, Freetype.FT_LOAD_RENDER)
+    for (const [code, glyph] of newGlyphs) {
+        const char = String.fromCodePoint(code)
+        cache.set(char, {
+            glyph,
+            bitmap: glyph.bitmap.imagedata
+                ? await createImageBitmap(glyph.bitmap.imagedata)
+                : null,
+        })
+    }
 
-  // TODO: Is awaiting with Promise.all faster? Is GPU uploading parallelizable?
+    // TODO: Is awaiting with Promise.all faster? Is GPU uploading parallelizable?
 }
 /**
  * @param {CanvasRenderingContext2D} ctx
@@ -76,60 +76,60 @@ async function updateCache(str, cache) {
  * @param {DrawCache} cache
  */
 export async function write(ctx, str, offsetx, offsety, cache) {
-  await updateCache(str, cache);
-  let prev = null;
-  for (const char of str) {
-    const { glyph, bitmap } = cache.get(char) || {};
-    if (glyph) {
-      // Kerning
-      if (prev) {
-        const kerning = Freetype.GetKerning(
-          prev.glyph_index,
-          glyph.glyph_index,
-          0
-        );
-        offsetx += kerning.x >> 6;
-      }
+    await updateCache(str, cache)
+    let prev = null
+    for (const char of str) {
+        const { glyph, bitmap } = cache.get(char) || {}
+        if (glyph) {
+            // Kerning
+            if (prev) {
+                const kerning = Freetype.GetKerning(
+                    prev.glyph_index,
+                    glyph.glyph_index,
+                    0
+                )
+                offsetx += kerning.x >> 6
+            }
 
-      if (bitmap) {
-        ctx.drawImage(
-          bitmap,
-          offsetx + glyph.bitmap_left,
-          offsety - glyph.bitmap_top
-        );
-      }
+            if (bitmap) {
+                ctx.drawImage(
+                    bitmap,
+                    offsetx + glyph.bitmap_left,
+                    offsety - glyph.bitmap_top
+                )
+            }
 
-      offsetx += glyph.advance.x >> 6;
-      prev = glyph;
+            offsetx += glyph.advance.x >> 6
+            prev = glyph
+        }
     }
-  }
 }
 
 // Create pixel perfect canvas
-const canvas = document.querySelector("canvas");
-const ctx = canvas?.getContext("2d");
+const canvas = document.querySelector("canvas")
+const ctx = canvas?.getContext("2d")
 if (!canvas || !ctx) {
-  throw new Error("No canvas or context found");
+    throw new Error("No canvas or context found")
 }
-canvas.width = window.innerWidth * window.devicePixelRatio;
-canvas.style.width = Math.floor(canvas.width / window.devicePixelRatio) + "px";
+canvas.width = window.innerWidth * window.devicePixelRatio
+canvas.style.width = Math.floor(canvas.width / window.devicePixelRatio) + "px"
 
-await createGoogleFont("Permanent+Marker", 0);
-const font = Freetype.SetFont("Permanent Marker", "Regular");
-const size = Freetype.SetPixelSize(0, 32 * window.devicePixelRatio);
-const cmap = Freetype.SetCharmap(Freetype.FT_ENCODING_UNICODE);
-const cache = new Map();
-const line_height = size.height >> 6;
+await createGoogleFont("Permanent+Marker", 0)
+const font = Freetype.SetFont("Permanent Marker", "Regular")
+const size = Freetype.SetPixelSize(0, 32 * window.devicePixelRatio)
+const cmap = Freetype.SetCharmap(Freetype.FT_ENCODING_UNICODE)
+const cache = new Map()
+const line_height = size.height >> 6
 if (!cmap) {
-  console.assert(false, "Unicode charmap is not found");
+    console.assert(false, "Unicode charmap is not found")
 }
-console.log("Font", font);
-console.log("Size", size);
-console.log("Charmap", cmap);
-await write(ctx, "LT. AVIATORS FOR THE WIN!", 0, line_height, cache);
-await write(ctx, "Lt. Aviators For The Win!", 0, line_height * 2, cache);
+console.log("Font", font)
+console.log("Size", size)
+console.log("Charmap", cmap)
+await write(ctx, "LT. AVIATORS FOR THE WIN!", 0, line_height, cache)
+await write(ctx, "Lt. Aviators For The Win!", 0, line_height * 2, cache)
 
 // NOTE: When changing font or size the cache must be cleared
 
-Freetype.UnloadFont("OSP-DIN");
-Freetype.Cleanup();
+Freetype.UnloadFont("OSP-DIN")
+Freetype.Cleanup()
--- test/test.js
+++ test/test.js
@@ -1,20 +1,20 @@
 // @ts-check
-import FreetypeInit from "../dist/freetype.js";
-const Freetype = await FreetypeInit();
+import FreetypeInit from "../dist/freetype.js"
+const Freetype = await FreetypeInit()
 
 async function createFontFromUrl(url) {
-  const font = await fetch(url);
-  const buffer = await font.arrayBuffer();
-  const face = Freetype.LoadFontFromBytes(new Uint8Array(buffer));
-  return face;
+    const font = await fetch(url)
+    const buffer = await font.arrayBuffer()
+    const face = Freetype.LoadFontFromBytes(new Uint8Array(buffer))
+    return face
 }
 
 async function createGoogleFont(fontName) {
-  const url = `https://fonts.googleapis.com/css?family=${fontName}&text=D`;
-  const css = await fetch(url);
-  const text = await css.text();
-  const urls = [...text.matchAll(/url\(([^\(\)]+)\)/g)].map((m) => m[1]);
-  return await createFontFromUrl(urls[0]);
+    const url = `https://fonts.googleapis.com/css?family=${fontName}&text=D`
+    const css = await fetch(url)
+    const text = await css.text()
+    const urls = [...text.matchAll(/url\(([^\(\)]+)\)/g)].map((m) => m[1])
+    return await createFontFromUrl(urls[0])
 }
 
 /**
@@ -22,100 +22,96 @@ async function createGoogleFont(fontName) {
  * @param {import("../dist/freetype.js").FT_GlyphSlotRec} glyph
  */
 function consoleDrawGlyph(glyph) {
-  const pixels = [""];
-  const data = glyph.bitmap.imagedata?.data;
-  const width = glyph.bitmap.imagedata?.width;
-  const height = glyph.bitmap.imagedata?.height;
-  if (!data || !width || !height) {
-    return;
-  }
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      const element = data[(y * width + x + 1) * 4 - 1];
-      if (element === undefined) {
-        continue;
-      } else if (element === 0) {
-        pixels.push(" ");
-      } else if (element > 200) {
-        pixels.push("#");
-      } else if (element < 128) {
-        pixels.push(".");
-      } else if (element < 200) {
-        pixels.push("o");
-      }
+    const pixels = [""]
+    const data = glyph.bitmap.imagedata?.data
+    const width = glyph.bitmap.imagedata?.width
+    const height = glyph.bitmap.imagedata?.height
+    if (!data || !width || !height) {
+        return
     }
-    pixels.push("\n");
-  }
-  console.log(pixels.join(""));
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const element = data[(y * width + x + 1) * 4 - 1]
+            if (element === undefined) {
+                continue
+            } else if (element === 0) {
+                pixels.push(" ")
+            } else if (element > 200) {
+                pixels.push("#")
+            } else if (element < 128) {
+                pixels.push(".")
+            } else if (element < 200) {
+                pixels.push("o")
+            }
+        }
+        pixels.push("\n")
+    }
+    console.log(pixels.join(""))
 }
 
-const font = await createGoogleFont("Karla");
-const font2 = await createGoogleFont("Karla");
-const setf = Freetype.SetFont("Karla", "Regular");
-const charm = Freetype.SetCharmap(Freetype.FT_ENCODING_UNICODE);
-const size = Freetype.SetPixelSize(16, 0);
-const chars = Freetype.LoadGlyphsFromCharmap(0, 9999, Freetype.FT_LOAD_RENDER);
+const font = await createGoogleFont("Karla")
+const font2 = await createGoogleFont("Karla")
+const setf = Freetype.SetFont("Karla", "Regular")
+const charm = Freetype.SetCharmap(Freetype.FT_ENCODING_UNICODE)
+const size = Freetype.SetPixelSize(16, 0)
+const chars = Freetype.LoadGlyphsFromCharmap(0, 9999, Freetype.FT_LOAD_RENDER)
 const charsmono = Freetype.LoadGlyphsFromCharmap(
-  0,
-  9999,
-  Freetype.FT_LOAD_RENDER |
-    Freetype.FT_LOAD_MONOCHROME |
-    Freetype.FT_LOAD_TARGET_MONO
-);
-const monod = charsmono.get(0x44); // 0x33 = letter D
-const chard = chars.get(0x44);
+    0,
+    9999,
+    Freetype.FT_LOAD_RENDER |
+        Freetype.FT_LOAD_MONOCHROME |
+        Freetype.FT_LOAD_TARGET_MONO
+)
+const monod = charsmono.get(0x44) // 0x33 = letter D
+const chard = chars.get(0x44)
 if (!chard || !monod) {
-  throw new Error("Glyphs not loaded");
+    throw new Error("Glyphs not loaded")
 }
 console.assert(
-  charm.encoding === Freetype.FT_ENCODING_UNICODE,
-  "🔴 Charmap not set",
-  charm
-);
+    charm.encoding === Freetype.FT_ENCODING_UNICODE,
+    "🔴 Charmap not set",
+    charm
+)
+console.assert(setf.family_name === "Karla", "🔴 Font set returned value", setf)
+console.assert(font[0].family_name === "Karla", "🔴 Font should load", font)
 console.assert(
-  setf.family_name === "Karla",
-  "🔴 Font set returned value",
-  setf
-);
-console.assert(font[0].family_name === "Karla", "🔴 Font should load", font);
+    font2[0].family_name === font[0].family_name,
+    "🔴 Font did not reload and replace",
+    font2
+)
+console.assert(size.x_ppem === 16, "🔴 Font size not proper", size)
+console.assert(chars.size > 1, "🔴 Antialized glyphs not loaded", chars.size)
 console.assert(
-  font2[0].family_name === font[0].family_name,
-  "🔴 Font did not reload and replace",
-  font2
-);
-console.assert(size.x_ppem === 16, "🔴 Font size not proper", size);
-console.assert(chars.size > 1, "🔴 Antialized glyphs not loaded", chars.size);
+    charsmono.size > 1,
+    "🔴 Monochrome glyphs not loaded",
+    chars.size
+)
 console.assert(
-  charsmono.size > 1,
-  "🔴 Monochrome glyphs not loaded",
-  chars.size
-);
+    chard?.bitmap.imagedata != null,
+    "🔴 Antialiased data does not exist"
+)
 console.assert(
-  chard?.bitmap.imagedata != null,
-  "🔴 Antialiased data does not exist"
-);
+    chard.bitmap.pixel_mode === 2,
+    "🔴 Gray mode is not enabled",
+    chard.bitmap.pixel_mode
+)
 console.assert(
-  chard.bitmap.pixel_mode === 2,
-  "🔴 Gray mode is not enabled",
-  chard.bitmap.pixel_mode
-);
+    monod?.bitmap.imagedata != null,
+    "🔴 Monochrome data does not exist"
+)
 console.assert(
-  monod?.bitmap.imagedata != null,
-  "🔴 Monochrome data does not exist"
-);
-console.assert(
-  monod.bitmap.pixel_mode === 1,
-  "🔴 Monochrome data is not monochrome",
-  monod.bitmap.pixel_mode
-);
+    monod.bitmap.pixel_mode === 1,
+    "🔴 Monochrome data is not monochrome",
+    monod.bitmap.pixel_mode
+)
 
-console.log("You should see an antialiaised letter D in the console:");
-consoleDrawGlyph(chard);
+console.log("You should see an antialiaised letter D in the console:")
+consoleDrawGlyph(chard)
 
-console.log("You should see an monochrome letter D in the console:");
-consoleDrawGlyph(monod);
+console.log("You should see an monochrome letter D in the console:")
+consoleDrawGlyph(monod)
 
-Freetype.UnloadFont("Karla");
-console.assert(null === Freetype.SetFont("Karla", "Regular"), " 🔴 Failure");
-Freetype.Cleanup();
-console.log("✅ Test finished");
+Freetype.UnloadFont("Karla")
+console.assert(null === Freetype.SetFont("Karla", "Regular"), " 🔴 Failure")
+Freetype.Cleanup()
+console.log("✅ Test finished")
